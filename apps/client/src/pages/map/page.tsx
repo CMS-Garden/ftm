2e8d6879f1f18090ee75a3b40e50c2dd9b867764
Phillip Bronzel <bronzel.phillip@gmail.com>
2024-03-16 16:42:00 +0100

    fix: map view

--- apps/client/src/pages/map/page.tsx
+++ apps/client/src/pages/map/page.tsx
@@ -1,9 +1,13 @@
 import { DonutChart } from '@shopify/polaris-viz';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 import styles from './style.module.css';
 import geoUrl from '../../assets/germany.geo.json?url';
+import { useState } from 'react';
 
 export default function Map() {
+  const [hovered, setHovered] = useState<string | null>(null);
+
   return (
     <div className={styles.test}>
       <DonutChart
@@ -48,11 +52,24 @@ export default function Map() {
         ]}
       />
       <hr />
-      <ComposableMap className={styles.map}>
+      <p>{hovered}</p>
+      <ComposableMap
+        className={styles.map}
+        projectionConfig={{
+          scale: 6500,
+          center: [10.5, 51.06],
+        }}
+        onMouseLeave={() => setHovered(null)}
+      >
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                onMouseEnter={(e) => setHovered(geo.properties.NAME_3)}
+                onClick={() => console.log(geo)}
+              />
             ))
           }
         </Geographies>
