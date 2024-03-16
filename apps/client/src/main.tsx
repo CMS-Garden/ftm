ab7fa9136ea94d9057f7ce591f5e5e9aa4c9dda8
Phillip Bronzel <bronzel.phillip@gmail.com>
2024-03-16 14:07:33 +0100

    chore: initial commit

---
+++ apps/client/src/main.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ apps/client/src/pages/index/page.tsx
@@ -0,0 +1,9 @@
+export default function Homepage() {
+  return (
+    <div className={styles.}>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore nulla
+      vitae dignissimos? Magnam architecto incidunt nemo nesciunt veniam ipsum
+      illo!
+    </div>
+  );
+}
