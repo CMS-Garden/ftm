42d8415f6b8fb38936fc3e8e59ba72854d590e6f
Phillip Bronzel <bronzel.phillip@gmail.com>
2024-03-16 15:26:27 +0100

    feat: add polaris charts

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import {
+  PolarisVizLightTheme,
+  PolarisVizProvider,
+  createTheme,
+} from '@shopify/polaris-viz';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Homepage from './pages/index/page';
 import Map from './pages/map/page';
 import 'the-new-css-reset/css/reset.css';
+import '@shopify/polaris-viz/build/esm/styles.css';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +24,20 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <PolarisVizProvider
+      defaultTheme="default"
+      themes={{
+        default: createTheme(
+          {
+            seriesColors: {
+              upToEight: ['#65a30d', '#16a34a', '#059669', '#0d9488'],
+            },
+          },
+          PolarisVizLightTheme
+        ),
+      }}
+    >
+      <RouterProvider router={router} />
+    </PolarisVizProvider>
   </React.StrictMode>
 );
