e40f5acc1bbbdf1d63cad3a301eae06ca52f4477
Moritz Bappert <mb@dreiqbik.de>
2024-03-17 15:52:45 +0100

    Styling changes

--- apps/client/src/components/NavBar.tsx
+++ apps/client/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import styles from './NavBar.module.css';
+import logoImg from '../assets/logo-small-negative.svg';
 
 export const NavBar = () => (
   <nav className={styles.navbar}>
     <ul>
       <Link to="/" className={styles.logo}>
-        <span>💸</span> Follow The Money
+        <img src={logoImg} height="50" alt="Follow the Money Logo" title="Follow the Money" />
       </Link>
       <div className={styles.links}>
         <a href="https://github.com/CMS-Garden/ftm">GitHub</a>
